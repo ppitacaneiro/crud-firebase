efccc80a08f8738bfa40f4807aa4ff970e4ba71d
pablo <ppitacaneiro@gmail.com>
2020-11-08 14:12:36 +0100

    Implementados métodos GET, POST, PUT, DELETE del CRUD contra Firebase

--- src/app/page/heroe/heroe.component.ts
+++ src/app/page/heroe/heroe.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+
 import { HeroeModel } from 'src/app/models/heroe.model';
 import { HeroesService } from 'src/app/services/heroes.service';
 
@@ -11,25 +14,38 @@ import { HeroesService } from 'src/app/services/heroes.service';
 export class HeroeComponent implements OnInit {
 
   heroe:HeroeModel = new HeroeModel();
+  peticionHttpOk:boolean = false;
 
-  constructor(private heroeService:HeroesService) { }
+  constructor(private heroeService:HeroesService, private route:ActivatedRoute) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+
+    if (id !== 'nuevo') {
+      this.heroeService.getGeroe(id).subscribe((response:HeroeModel) => {
+        this.heroe = response;
+        this.heroe.id = id;
+      });
+    }
   }
 
   save(form:NgForm) {
+
+    let peticionHttp: Observable<any>;
 
     if (form.invalid) {
       console.log('Formulario invalido');
       return;
     }
 
-    console.log(form);
-    console.log(this.heroe);
+    if (this.heroe.id) {
+      peticionHttp = this.heroeService.updateHeroe(this.heroe);
+    } else {
+      peticionHttp = this.heroeService.createHeroe(this.heroe);
+    }
 
-    this.heroeService.createHeroe(this.heroe).subscribe( response => {
-      console.log(response);
+    peticionHttp.subscribe( reponse => {
+      this.peticionHttpOk = true;
     });
   }
-
 }
--- src/app/page/heroes/heroes.component.ts
+++ src/app/page/heroes/heroes.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ɵConsole } from '@angular/core';
+import { HeroeModel } from 'src/app/models/heroe.model';
+import { HeroesService } from 'src/app/services/heroes.service';
 
 @Component({
   selector: 'app-heroes',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  heroes:HeroeModel[] = [];
+  loading:boolean = false;
+
+  constructor(private heroesService:HeroesService) { }
 
   ngOnInit(): void {
+    console.log(this.heroes.length);
+    this.loading = true;
+    this.heroesService.getHeroes()
+    .subscribe(response => {
+      this.heroes = response;
+      this.loading = false;
+    });
+  }
+
+  deleteHeroe(heroe:HeroeModel,i:number) {
+    this.heroesService.deleteHeroe(heroe.id).subscribe();
+    this.heroes.splice(i,1);
   }
 
 }
